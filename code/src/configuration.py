e3993c7be2734a20f2f0f1f56bb4a73289e430c7
vsurendrav <surendra.kmr3@gmail.com>
2025-03-26 09:28:15 +0530

    configuration file

---
+++ code/src/configuration.py
@@ -0,0 +1,11 @@
+requestTypesAndSubTypes = {
+		"Adjustment": ["Reallocation Fees", "Amendment Fees", "Reallocation Principal"],
+		"AU Transfer": [],
+		"Closing Notice": ["Cashless Roll", "Decrease", "Increase"],
+		"Commitment Change": ["Ongoing Fee", "Letter of Credit Fee"],
+		"Fee Payment": ["Principal", "Interest", "Principal + Interest", "Principal + Interest + Fee"],
+		"Money Movement - Inbound": ["Timebound"],
+		"Money Movement - Outbound": ["Foreign Currency"]
+	};
+keyMetrics = ["sender name", "amount", "expiration date", "and date to transfer", "account number"]
+ 
